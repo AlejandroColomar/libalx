304d51f71bd35fbc65de8835d32bb03650dfe5c0
Alejandro Colomar Andrés <colomar.6.4.3@gmail.com>
2019-04-21 13:24:39 +0200

    Test for __int128 availability

--- inc/libalx/base/stdint/types.hpp
+++ inc/libalx/base/stdint/types.hpp
@@ -21,22 +21,25 @@
  ******* typedefs *************************************************************
  ******************************************************************************/
 typedef unsigned char		uchar;
-/* This is to test if uint128_t already exists */
-#if !defined(UINT128_MAX)
+#if defined(__SIZEOF_INT128__)
+# if !defined(UINT128_MAX)	/* This is to test if uint128_t already exists */
 typedef	unsigned __int128	uint128_t;
 typedef	__int128		int128_t;
+# endif
 #endif
 
 
 /******************************************************************************
  ******* macros ***************************************************************
  ******************************************************************************/
+#if defined(__SIZEOF_INT128__)
 #define U128_MAX	(~((uint128_t)0))
 #define S128_MAX	((int128_t)(U128_MAX >> 1))
 #define S128_MIN	((int128_t)(-S128_MAX - 1))
 
 #define SQRT_U128_MAX	((uint128_t)(0xFFFFFFFFFFFFFFFF))
 #define SQRT_S128_MAX	((int128_t)(0xB504F333F9DE6800))
+#endif
 
 
 /******************************************************************************
