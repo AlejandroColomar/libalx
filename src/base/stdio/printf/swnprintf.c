47e515b5fcde557285dd62caa411825e83f7c8d9
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-07-02 04:14:53 +0200

    rename sbprintf__ -> swnprintf; cxx implementation too

--- src/base/stdio/printf/swnprintf.c
+++ src/base/stdio/printf/swnprintf.c
@@ -7,7 +7,11 @@
 /******************************************************************************
  ******* headers **************************************************************
  ******************************************************************************/
-#include "libalx/base/stdio/printf/sbprintf.h"
+#include "libalx/base/stdio/printf/swnprintf.h"
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 
 /******************************************************************************
@@ -38,6 +42,33 @@
 /******************************************************************************
  ******* global functions *****************************************************
  ******************************************************************************/
+int	alx_swnprintf(char str[restrict], int *restrict written, size_t nmemb,
+			char *restrict format, ...)
+{
+	va_list	ap;
+	int	len;
+
+	va_start(ap, format);
+	len	= vsnprintf(str, nmemb, format, ap);
+	va_end(ap);
+
+	if (written != NULL)
+		*written = len;
+
+	if (len < 0)
+		goto err;
+	if ((unsigned)len >= nmemb)
+		goto trunc;
+
+	return	0;
+err:
+	return	-errno;
+trunc:
+	if (written)
+		*written = nmemb - 1;
+	errno	= ENOMEM;
+	return	ENOMEM;
+}
 
 
 /******************************************************************************
