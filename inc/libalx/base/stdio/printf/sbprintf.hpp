0f50355a0a2abe36a8f8917a85ca0ea23c5794ab
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-07-03 01:59:56 +0200

    Fix alx_sbprintf (c++)

--- inc/libalx/base/stdio/printf/sbprintf.hpp
+++ inc/libalx/base/stdio/printf/sbprintf.hpp
@@ -29,7 +29,7 @@ namespace alx {
  ******************************************************************************/
 /*
  * int	alx_sbprintf(char buff[restrict], ptrdiff_t *restrict written,
- *				const char *restrict fmt, ...);
+ *		     const char *restrict fmt, ...);
  */
 #define alx_sbprintf(buff, written, fmt, ...)	(			\
 {									\
