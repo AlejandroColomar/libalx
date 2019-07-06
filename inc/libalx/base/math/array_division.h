66b7ae816a9f4244ab969a4400a330118ce84e72
Alejandro Colomar <Colomar.6.4.3@GMail.com>
2019-07-06 03:48:22 +0200

    non restrict

--- inc/libalx/base/math/array_division.h
+++ inc/libalx/base/math/array_division.h
@@ -43,72 +43,72 @@
  ******************************************************************************/
 inline
 void	alx_array_division_ldbl	(ptrdiff_t nmemb,
-				 long double dest[static restrict nmemb],
-				 const long double src1[static restrict nmemb],
-				 const long double src2[static restrict nmemb])
+				 long double dest[static nmemb],
+				 const long double src1[static nmemb],
+				 const long double src2[static nmemb])
 __attribute__((nonnull));
 inline
 void	alx_array_division	(ptrdiff_t nmemb,
-				 double dest[static restrict nmemb],
-				 const double src1[static restrict nmemb],
-				 const double src2[static restrict nmemb])
+				 double dest[static nmemb],
+				 const double src1[static nmemb],
+				 const double src2[static nmemb])
 __attribute__((nonnull));
 inline
 void	alx_array_division_flt	(ptrdiff_t nmemb,
-				 float dest[static restrict nmemb],
-				 const float src1[static restrict nmemb],
-				 const float src2[static restrict nmemb])
+				 float dest[static nmemb],
+				 const float src1[static nmemb],
+				 const float src2[static nmemb])
 __attribute__((nonnull));
 
 int	alx_array_division_uint	(ptrdiff_t nmemb,
-				 unsigned dest[static restrict nmemb],
-				 const unsigned src1[static restrict nmemb],
-				 const unsigned src2[static restrict nmemb])
+				 unsigned dest[static nmemb],
+				 const unsigned src1[static nmemb],
+				 const unsigned src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_int	(ptrdiff_t nmemb,
-				 int dest[static restrict nmemb],
-				 const int src1[static restrict nmemb],
-				 const int src2[static restrict nmemb])
+				 int dest[static nmemb],
+				 const int src1[static nmemb],
+				 const int src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_u8	(ptrdiff_t nmemb,
-				 uint8_t dest[static restrict nmemb],
-				 const uint8_t src1[static restrict nmemb],
-				 const uint8_t src2[static restrict nmemb])
+				 uint8_t dest[static nmemb],
+				 const uint8_t src1[static nmemb],
+				 const uint8_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_s8	(ptrdiff_t nmemb,
-				 int8_t dest[static restrict nmemb],
-				 const int8_t src1[static restrict nmemb],
-				 const int8_t src2[static restrict nmemb])
+				 int8_t dest[static nmemb],
+				 const int8_t src1[static nmemb],
+				 const int8_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_u16	(ptrdiff_t nmemb,
-				 uint16_t dest[static restrict nmemb],
-				 const uint16_t src1[static restrict nmemb],
-				 const uint16_t src2[static restrict nmemb])
+				 uint16_t dest[static nmemb],
+				 const uint16_t src1[static nmemb],
+				 const uint16_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_s16	(ptrdiff_t nmemb,
-				 int16_t dest[static restrict nmemb],
-				 const int16_t src1[static restrict nmemb],
-				 const int16_t src2[static restrict nmemb])
+				 int16_t dest[static nmemb],
+				 const int16_t src1[static nmemb],
+				 const int16_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_u32	(ptrdiff_t nmemb,
-				 uint32_t dest[static restrict nmemb],
-				 const uint32_t src1[static restrict nmemb],
-				 const uint32_t src2[static restrict nmemb])
+				 uint32_t dest[static nmemb],
+				 const uint32_t src1[static nmemb],
+				 const uint32_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_s32	(ptrdiff_t nmemb,
-				 int32_t dest[static restrict nmemb],
-				 const int32_t src1[static restrict nmemb],
-				 const int32_t src2[static restrict nmemb])
+				 int32_t dest[static nmemb],
+				 const int32_t src1[static nmemb],
+				 const int32_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_u64	(ptrdiff_t nmemb,
-				 uint64_t dest[static restrict nmemb],
-				 const uint64_t src1[static restrict nmemb],
-				 const uint64_t src2[static restrict nmemb])
+				 uint64_t dest[static nmemb],
+				 const uint64_t src1[static nmemb],
+				 const uint64_t src2[static nmemb])
 __attribute__((nonnull));
 int	alx_array_division_s64	(ptrdiff_t nmemb,
-				 int64_t dest[static restrict nmemb],
-				 const int64_t src1[static restrict nmemb],
-				 const int64_t src2[static restrict nmemb])
+				 int64_t dest[static nmemb],
+				 const int64_t src1[static nmemb],
+				 const int64_t src2[static nmemb])
 __attribute__((nonnull));
 
 
@@ -117,9 +117,9 @@ __attribute__((nonnull));
  ******************************************************************************/
 inline
 void	alx_array_division_ldbl	(ptrdiff_t nmemb,
-				 long double dest[static restrict nmemb],
-				 const long double src1[static restrict nmemb],
-				 const long double src2[static restrict nmemb])
+				 long double dest[static nmemb],
+				 const long double src1[static nmemb],
+				 const long double src2[static nmemb])
 {
 
 	for (ptrdiff_t i = 0; i < nmemb; i++)
@@ -128,9 +128,9 @@ void	alx_array_division_ldbl	(ptrdiff_t nmemb,
 
 inline
 void	alx_array_division	(ptrdiff_t nmemb,
-				 double dest[static restrict nmemb],
-				 const double src1[static restrict nmemb],
-				 const double src2[static restrict nmemb])
+				 double dest[static nmemb],
+				 const double src1[static nmemb],
+				 const double src2[static nmemb])
 {
 
 	for (ptrdiff_t i = 0; i < nmemb; i++)
@@ -139,9 +139,9 @@ void	alx_array_division	(ptrdiff_t nmemb,
 
 inline
 void	alx_array_division_flt	(ptrdiff_t nmemb,
-				 float dest[static restrict nmemb],
-				 const float src1[static restrict nmemb],
-				 const float src2[static restrict nmemb])
+				 float dest[static nmemb],
+				 const float src1[static nmemb],
+				 const float src2[static nmemb])
 {
 
 	for (ptrdiff_t i = 0; i < nmemb; i++)
