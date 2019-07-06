24d58edeaae02bf1cdc033fd4282fc5947cca13c
Alejandro Colomar <Colomar.6.4.3@GMail.com>
2019-07-06 18:58:10 +0200

    C99 inline

--- src/base/stdio/seekc.c
+++ src/base/stdio/seekc.c
@@ -40,32 +40,12 @@
 /******************************************************************************
  ******* global functions *****************************************************
  ******************************************************************************/
-int	alx_seekc(char c)
-{
-	int ch;
-
-	do {
-		ch = getchar();
-	} while ((ch != c)  &&  (ch != EOF));
-
-	return	(ch == EOF);
-}
-
-int	alx_fseekc(FILE *stream, char c)
-{
-	int ch;
-
-	do {
-		ch = fgetc(stream);
-	} while ((ch != c)  &&  (ch != EOF));
-
-	return	(ch == EOF);
-}
-
-int	alx_wait4enter(void)
-{
-	return	alx_seekc('\n');
-}
+extern
+int	alx_seekc(char c);
+extern
+int	alx_fseekc(FILE *stream, char c);
+extern
+int	alx_wait4enter(void);
 
 
 /******************************************************************************
