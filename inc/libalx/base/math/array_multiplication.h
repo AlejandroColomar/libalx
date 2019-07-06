958ac1f1ed69659a71b7ce406a9d1b40d922ae40
Alejandro Colomar <Colomar.6.4.3@GMail.com>
2019-07-06 03:44:51 +0200

    C99 inline; static; non-restrict, attribute nonnull

--- inc/libalx/base/math/array_multiplication.h
+++ inc/libalx/base/math/array_multiplication.h
@@ -39,97 +39,109 @@
 
 
 /******************************************************************************
- ******* extern functions *****************************************************
+ ******* function prototypes **************************************************
  ******************************************************************************/
-int	alx_array_multiplication_uint	(ptrdiff_t nmemb,
-					unsigned dest[restrict nmemb],
-					const unsigned src1[nmemb],
-					const unsigned src2[nmemb]);
-int	alx_array_multiplication_int	(ptrdiff_t nmemb,
-					int dest[restrict nmemb],
-					const int src1[nmemb],
-					const int src2[nmemb]);
-int	alx_array_multiplication_u8	(ptrdiff_t nmemb,
-					uint8_t dest[restrict nmemb],
-					const uint8_t src1[nmemb],
-					const uint8_t src2[nmemb]);
-int	alx_array_multiplication_s8	(ptrdiff_t nmemb,
-					int8_t dest[restrict nmemb],
-					const int8_t src1[nmemb],
-					const int8_t src2[nmemb]);
-int	alx_array_multiplication_u16	(ptrdiff_t nmemb,
-					uint16_t dest[restrict nmemb],
-					const uint16_t src1[nmemb],
-					const uint16_t src2[nmemb]);
-int	alx_array_multiplication_s16	(ptrdiff_t nmemb,
-					int16_t dest[restrict nmemb],
-					const int16_t src1[nmemb],
-					const int16_t src2[nmemb]);
-int	alx_array_multiplication_u32	(ptrdiff_t nmemb,
-					uint32_t dest[restrict nmemb],
-					const uint32_t src1[nmemb],
-					const uint32_t src2[nmemb]);
-int	alx_array_multiplication_s32	(ptrdiff_t nmemb,
-					int32_t dest[restrict nmemb],
-					const int32_t src1[nmemb],
-					const int32_t src2[nmemb]);
-int	alx_array_multiplication_u64	(ptrdiff_t nmemb,
-					uint64_t dest[restrict nmemb],
-					const uint64_t src1[nmemb],
-					const uint64_t src2[nmemb]);
-int	alx_array_multiplication_s64	(ptrdiff_t nmemb,
-					int64_t dest[restrict nmemb],
-					const int64_t src1[nmemb],
-					const int64_t src2[nmemb]);
-
-
-/******************************************************************************
- ******* static inline functions (prototypes) *********************************
- ******************************************************************************/
-static inline	void	alx_array_multiplication_ldbl	(ptrdiff_t nmemb,
-					long double dest[nmemb],
-					const long double src1[nmemb],
-					const long double src2[nmemb]);
-static inline	void	alx_array_multiplication	(ptrdiff_t nmemb,
-					double dest[nmemb],
-					const double src1[nmemb],
-					const double src2[nmemb]);
-static inline	void	alx_array_multiplication_flt	(ptrdiff_t nmemb,
-					float dest[nmemb],
-					const float src1[nmemb],
-					const float src2[nmemb]);
-
-
-/******************************************************************************
- ******* static inline functions (definitions) ********************************
- ******************************************************************************/
-static inline
+inline
 void	alx_array_multiplication_ldbl	(ptrdiff_t nmemb,
-					long double dest[nmemb],
-					const long double src1[nmemb],
-					const long double src2[nmemb])
-{
-
-	for (ptrdiff_t i = 0; i < nmemb; i++)
-		dest[i]	= src1[i] * src2[i];
-}
-
-static inline
+					 long double dest[static nmemb],
+					 const long double src1[static nmemb],
+					 const long double src2[static nmemb])
+__attribute__((nonnull));
+inline
 void	alx_array_multiplication	(ptrdiff_t nmemb,
-					double dest[nmemb],
-					const double src1[nmemb],
-					const double src2[nmemb])
+					 double dest[static nmemb],
+					 const double src1[static nmemb],
+					 const double src2[static nmemb])
+__attribute__((nonnull));
+inline
+void	alx_array_multiplication_flt	(ptrdiff_t nmemb,
+					 float dest[static nmemb],
+					 const float src1[static nmemb],
+					 const float src2[static nmemb])
+__attribute__((nonnull));
+
+int	alx_array_multiplication_uint	(ptrdiff_t nmemb,
+					 unsigned dest[static nmemb],
+					 const unsigned src1[static nmemb],
+					 const unsigned src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_int	(ptrdiff_t nmemb,
+					 int dest[static nmemb],
+					 const int src1[static nmemb],
+					 const int src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_u8	(ptrdiff_t nmemb,
+					 uint8_t dest[static nmemb],
+					 const uint8_t src1[static nmemb],
+					 const uint8_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_s8	(ptrdiff_t nmemb,
+					 int8_t dest[static nmemb],
+					 const int8_t src1[static nmemb],
+					 const int8_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_u16	(ptrdiff_t nmemb,
+					 uint16_t dest[static nmemb],
+					 const uint16_t src1[static nmemb],
+					 const uint16_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_s16	(ptrdiff_t nmemb,
+					 int16_t dest[static nmemb],
+					 const int16_t src1[static nmemb],
+					 const int16_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_u32	(ptrdiff_t nmemb,
+					 uint32_t dest[static nmemb],
+					 const uint32_t src1[static nmemb],
+					 const uint32_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_s32	(ptrdiff_t nmemb,
+					 int32_t dest[static nmemb],
+					 const int32_t src1[static nmemb],
+					 const int32_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_u64	(ptrdiff_t nmemb,
+					 uint64_t dest[static nmemb],
+					 const uint64_t src1[static nmemb],
+					 const uint64_t src2[static nmemb])
+__attribute__((nonnull));
+int	alx_array_multiplication_s64	(ptrdiff_t nmemb,
+					 int64_t dest[static nmemb],
+					 const int64_t src1[static nmemb],
+					 const int64_t src2[static nmemb])
+__attribute__((nonnull));
+
+
+/******************************************************************************
+ ******* inline functions *****************************************************
+ ******************************************************************************/
+inline
+void	alx_array_multiplication_ldbl	(ptrdiff_t nmemb,
+					 long double dest[static nmemb],
+					 const long double src1[static nmemb],
+					 const long double src2[static nmemb])
 {
 
 	for (ptrdiff_t i = 0; i < nmemb; i++)
 		dest[i]	= src1[i] * src2[i];
 }
 
-static inline
+inline
+void	alx_array_multiplication	(ptrdiff_t nmemb,
+					 double dest[static nmemb],
+					 const double src1[static nmemb],
+					 const double src2[static nmemb])
+{
+
+	for (ptrdiff_t i = 0; i < nmemb; i++)
+		dest[i]	= src1[i] * src2[i];
+}
+
+inline
 void	alx_array_multiplication_flt	(ptrdiff_t nmemb,
-					float dest[nmemb],
-					const float src1[nmemb],
-					const float src2[nmemb])
+					 float dest[static nmemb],
+					 const float src1[static nmemb],
+					 const float src2[static nmemb])
 {
 
 	for (ptrdiff_t i = 0; i < nmemb; i++)
