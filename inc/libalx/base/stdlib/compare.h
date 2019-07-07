e6de58a5d819c9c4d6cbbf5275b534034b725872
Alejandro Colomar <Colomar.6.4.3@GMail.com>
2019-07-07 07:24:44 +0200

    attribute pure

--- inc/libalx/base/stdlib/compare.h
+++ inc/libalx/base/stdlib/compare.h
@@ -42,43 +42,43 @@
  ******************************************************************************/
 inline
 int	alx_compare_ldbl(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline	
 int	alx_compare	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_f	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_uint(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_int	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_u8	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_s8	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_u16	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_s16	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_u32	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_s32	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_u64	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 inline
 int	alx_compare_s64	(const void *a_ptr, const void *b_ptr)
-__attribute__((nonnull));
+__attribute__((nonnull, pure));
 
 
 /******************************************************************************
