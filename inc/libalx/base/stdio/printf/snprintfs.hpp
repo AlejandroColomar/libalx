3998d849193b55b9ae9f8534b9d0584e160ee543
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-07-08 19:01:08 +0200

    snprintfs: warn_unused_result

--- inc/libalx/base/stdio/printf/snprintfs.hpp
+++ inc/libalx/base/stdio/printf/snprintfs.hpp
@@ -33,11 +33,13 @@ extern	"C"
 int	alx_snprintfs	(char *restrict str,
 			 ptrdiff_t *restrict written, ptrdiff_t nmemb,
 			 const char *restrict format, ...)
-	__attribute__((nonnull(1, 4), format(printf, 4, 5)));
+	__attribute__((nonnull(1, 4), format(printf, 4, 5),
+		       warn_unused_result));
 int	alx_vsnprintfs	(char *restrict str,
 			 ptrdiff_t *restrict written, ptrdiff_t nmemb,
 			 const char *restrict format, va_list ap)
-	__attribute__((nonnull(1, 4), format(printf, 4, 0)));
+	__attribute__((nonnull(1, 4), format(printf, 4, 0),
+		       warn_unused_result));
 }
 
 
