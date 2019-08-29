6d602231e17b0e9bf1729fa8fee9db11933763b8
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-08-29 13:17:14 +0200

    Add static assertions for stdint types & char;  Fix static assertions so that they can be used outside of functions

--- inc/libalx/base/assert/assert.h
+++ inc/libalx/base/assert/assert.h
@@ -13,7 +13,7 @@
 /******************************************************************************
  ******* headers **************************************************************
  ******************************************************************************/
-#include <assert.h>
+#include <limits.h>
 
 #include "libalx/base/compiler/type.h"
 
@@ -21,18 +21,46 @@
 /******************************************************************************
  ******* macros ***************************************************************
  ******************************************************************************/
-#define alx_static_assert_array(a)	do				\
-{									\
-									\
-	static_assert(!alx_same_type((a), &(a)[0]), "Not a `[]` !");	\
-} while (0)
+#define alx_Static_assert_array(a)					\
+	_Static_assert(!alx_same_type((a), &(a)[0]), "Not a `[]` !")
 
-#define alx_static_assert_char_array(a)	do				\
-{									\
-	alx_static_assert_array(a);					\
-	static_assert(__builtin_types_compatible_p(char, typeof((a)[0])),\
-						"Not a `char[]` !");	\
-} while (0)
+#define alx_Static_assert_char_array(a)					\
+	alx_Static_assert_array(a);					\
+	_Static_assert(__builtin_types_compatible_p(			\
+			char, typeof((a)[0])),				\
+			"Not a `char[]` !")
+
+#define alx_Static_assert_char_signed()					\
+	_Static_assert(CHAR_MIN < 0, "char != signed char")
+
+#define alx_Static_assert_char_unsigned()				\
+	_Static_assert(CHAR_MIN == 0, "char != unsigned char")
+
+#define alx_Static_assert_stdint_types()				\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 uint8_t, unsigned char),			\
+			"uint8_t != unsigned char");			\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 int8_t, signed char),				\
+			"int8_t != signed char");			\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 uint16_t, unsigned short),			\
+			"uint16_t != unsigned short");			\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 int16_t, short),				\
+			"int16_t != short");				\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 uint32_t, unsigned),				\
+			"uint32_t != unsigned");			\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 int32_t, int),					\
+			"int32_t != int");				\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 uint64_t, unsigned long),			\
+			"uint64_t != unsigned long");			\
+	_Static_assert(__builtin_types_compatible_p(			\
+			 int64_t, long),				\
+			"int64_t != long")
 
 
 /******************************************************************************
--- inc/libalx/base/assert/assert.hpp
+++ inc/libalx/base/assert/assert.hpp
@@ -13,7 +13,7 @@
 /******************************************************************************
  ******* headers **************************************************************
  ******************************************************************************/
-#include <cassert>
+#include <climits>
 
 #include <type_traits>
 
@@ -21,19 +21,37 @@
 /******************************************************************************
  ******* macros ***************************************************************
  ******************************************************************************/
-#define alx_static_assert_array(a)	do				\
-{									\
-									\
-	static_assert(std::is_array <typeof(a)>::value, "Not a `[]`!");	\
-} while (0)
+#define alx_Static_assert_array(a)					\
+	_Static_assert(std::is_array <typeof(a)>::value, "Not a `[]`!")
 
-#define alx_static_assert_char_array(a)	do				\
-{									\
-									\
-	alx_static_assert_array(a);					\
-	static_assert(std::is_same <char, typeof((a)[0])>::value,	\
-						"Not a `char[]` !");	\
-} while (0)
+#define alx_static_assert_char_array(a)					\
+	alx_Static_assert_array(a);					\
+	_Static_assert(std::is_same <char, typeof((a)[0])>::value,	\
+						"Not a `char[]` !")
+
+#define alx_Static_assert_char_signed()					\
+	_Static_assert(CHAR_MIN < 0, "char != signed char")
+
+#define alx_Static_assert_char_unsigned()				\
+	_Static_assert(CHAR_MIN == 0, "char != unsigned char")
+
+#define alx_Static_assert_stdint_types()				\
+	_Static_assert(std::is_same <uint8_t, unsigned char>::value,	\
+				    "uint8_t != unsigned char");	\
+	_Static_assert(std::is_same < int8_t, signed char>::value,	\
+				     "int8_t != signed char");		\
+	_Static_assert(std::is_same <uint16_t, unsigned short>::value,	\
+				    "uint16_t != unsigned short");	\
+	_Static_assert(std::is_same < int16_t, short>::value,		\
+				     "int16_t != short");		\
+	_Static_assert(std::is_same <uint32_t, unsigned>::value,	\
+				    "uint32_t != unsigned");		\
+	_Static_assert(std::is_same < int32_t, int>::value,		\
+				     "int32_t != int");			\
+	_Static_assert(std::is_same <uint64_t, unsigned long>::value,	\
+				    "uint64_t != unsigned long");	\
+	_Static_assert(std::is_same < int64_t, long>::value,		\
+				     "int64_t != long")
 
 
 /******************************************************************************
