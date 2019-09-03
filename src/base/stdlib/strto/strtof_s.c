5f2bf8b26c37f6986ebd1aa9aff168bd18e38af8
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-09-03 16:49:45 +0200

    strtof_s(): Fix errno bug

--- src/base/stdlib/strto/strtof_s.c
+++ src/base/stdlib/strto/strtof_s.c
@@ -26,7 +26,8 @@
 __attribute__((nonnull, warn_unused_result))
 static
 int	alx_strtof_status	(const char *restrict str,
-				 const char *restrict endptr, int _errno);
+				 const char *restrict endptr,
+				 int _errno, int _errno_before);
 
 
 /******************************************************************************
@@ -35,37 +36,43 @@ int	alx_strtof_status	(const char *restrict str,
 int	alx_strtod_s	(double *restrict num, const char *restrict str,
 			 ptrdiff_t *restrict read)
 {
-	char	*endptr;
+	const int	_errno = errno;
+	char		*endptr;
 
+	errno	= 0;
 	*num	= strtod(str, &endptr);
 	if (read)
 		*read	= endptr - str;
 
-	return	alx_strtof_status(str, endptr, errno);
+	return	alx_strtof_status(str, endptr, errno, _errno);
 }
 
 int	alx_strtof_s	(float *restrict num, const char *restrict str,
 			 ptrdiff_t *restrict read)
 {
-	char	*endptr;
+	const int	_errno = errno;
+	char		*endptr;
 
+	errno	= 0;
 	*num	= strtof(str, &endptr);
 	if (read)
 		*read	= endptr - str;
 
-	return	alx_strtof_status(str, endptr, errno);
+	return	alx_strtof_status(str, endptr, errno, _errno);
 }
 
 int	alx_strtold_s	(long double *restrict num, const char *restrict str,
 			 ptrdiff_t *restrict read)
 {
-	char	*endptr;
+	const int	_errno = errno;
+	char		*endptr;
 
+	errno	= 0;
 	*num	= strtold(str, &endptr);
 	if (read)
 		*read	= endptr - str;
 
-	return	alx_strtof_status(str, endptr, errno);
+	return	alx_strtof_status(str, endptr, errno, _errno);
 }
 
 
@@ -74,17 +81,32 @@ int	alx_strtold_s	(long double *restrict num, const char *restrict str,
  ******************************************************************************/
 static
 int	alx_strtof_status	(const char *restrict str,
-				 const char *restrict endptr, int _errno)
+				 const char *restrict endptr,
+				 int _errno, int _errno_before)
 {
+	int	status;
 
-	if (_errno == ERANGE)
-		return	ERANGE;
-	if (*endptr != '\0')
-		return	ENOTSUP;
-	if (str == endptr)
-		return	-ECANCELED;
+	status	= 0;
 
-	return	0;
+	if (str == endptr) {
+		status	= -ECANCELED;
+		goto out;
+	}
+
+	if (*endptr != '\0') {
+		status	= ENOTSUP;
+		goto out;
+	}
+
+	if (_errno == ERANGE) {
+		status	= ERANGE;
+		goto out;
+	}
+out:
+	if (!errno)
+		errno	= _errno_before;
+
+	return	status;
 }
 
 
