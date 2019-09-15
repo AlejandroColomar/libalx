6456ab2eb4ae6cd8c343754100bd3871bf6f72cb
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-09-15 12:40:08 +0200

    base: stdlib: Separate search and bsearch

--- inc/libalx/base/stdlib/bsearch.hpp
+++ inc/libalx/base/stdlib/bsearch.hpp
@@ -7,7 +7,7 @@
 /******************************************************************************
  ******* include guard ********************************************************
  ******************************************************************************/
-#pragma once	/* libalx/base/stdlib/search.hpp */
+#pragma once	/* libalx/base/stdlib/bsearch.hpp */
 
 
 /******************************************************************************
@@ -25,43 +25,43 @@
 extern	"C"
 {
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_uint	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_uint(ptrdiff_t nmemb,
 				 const unsigned *restrict arr,
 				 unsigned x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_int	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_int	(ptrdiff_t nmemb,
 				 const int *restrict arr,
 				 int x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_u8	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_u8	(ptrdiff_t nmemb,
 				 const uint8_t *restrict arr,
 				 uint8_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_s8	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_s8	(ptrdiff_t nmemb,
 				 const int8_t *restrict arr,
 				 int8_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_u16	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_u16	(ptrdiff_t nmemb,
 				 const uint16_t *restrict arr,
 				 uint16_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_s16	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_s16	(ptrdiff_t nmemb,
 				 const int16_t *restrict arr,
 				 int16_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_u32	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_u32	(ptrdiff_t nmemb,
 				 const uint32_t *restrict arr,
 				 uint32_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_s32	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_s32	(ptrdiff_t nmemb,
 				 const int32_t *restrict arr,
 				 int32_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_u64	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_u64	(ptrdiff_t nmemb,
 				 const uint64_t *restrict arr,
 				 uint64_t x);
 [[gnu::nonnull]][[gnu::pure]]
-ptrdiff_t	alx_search_s64	(ptrdiff_t nmemb,
+ptrdiff_t	alx_bsearch_s64	(ptrdiff_t nmemb,
 				 const int64_t *restrict arr,
 				 int64_t x);
 }
