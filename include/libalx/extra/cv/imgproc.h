06711e061a23b441a99af664da68bcf23531f3ae
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-10-02 01:43:45 +0200

    Add enum with OpenCV values unavaliable in C since OpenCV4

--- include/libalx/extra/cv/imgproc.h
+++ include/libalx/extra/cv/imgproc.h
@@ -290,6 +290,20 @@ enum	Alx_Cv_ColorConversionCodes {
 	ALX_CV_COLOR_COLORCVT_MAX  = 139
 };
 
+enum	Alx_Cv_ColorConversionCodes {
+	ALX_CV_THRESH_BINARY      = 0,  /**< value = value > threshold ? max_value : 0       */
+	ALX_CV_THRESH_BINARY_INV  = 1,  /**< value = value > threshold ? 0 : max_value       */
+	ALX_CV_THRESH_TRUNC       = 2,  /**< value = value > threshold ? threshold : value   */
+	ALX_CV_THRESH_TOZERO      = 3,  /**< value = value > threshold ? value : 0           */
+	ALX_CV_THRESH_TOZERO_INV  = 4,  /**< value = value > threshold ? 0 : value           */
+	ALX_CV_THRESH_MASK        = 7,
+	ALX_CV_THRESH_OTSU        = 8, /**< use Otsu algorithm to choose the optimal threshold value;
+					combine the flag with one of the above CV_THRESH_* values */
+	ALX_CV_THRESH_TRIANGLE    = 16  /**< use Triangle algorithm to choose the optimal threshold value;
+					combine the flag with one of the above CV_THRESH_* values, but not
+					with CV_THRESH_OTSU */
+};
+
 
 /******************************************************************************
  ******* struct / union *******************************************************
