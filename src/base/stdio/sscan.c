666f33dc6f4b68e47efd5eec06160b37030e7e31
alejandro-colomar <colomar.6.4.3@gmail.com>
2019-12-14 14:32:05 +0100

    Cosmetic

--- src/base/stdio/sscan.c
+++ src/base/stdio/sscan.c
@@ -39,8 +39,8 @@
  ******* global functions *****************************************************
  ******************************************************************************/
 int	alx_sscan_ldbl	(long double *restrict dest,
-			long double m, long double def, long double M,
-			const char *restrict str)
+			 long double m, long double def, long double M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %Lf", dest) != 1)
@@ -58,8 +58,8 @@ err_sscanf:
 }
 
 int	alx_sscan_dbl	(double *restrict dest,
-			double m, double def, double M,
-			const char *restrict str)
+			 double m, double def, double M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %lf", dest) != 1)
@@ -77,8 +77,8 @@ err_sscanf:
 }
 
 int	alx_sscan_flt	(float *restrict dest,
-			float m, float def, float M,
-			const char *restrict str)
+			 float m, float def, float M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %f", dest) != 1)
@@ -96,8 +96,8 @@ err_sscanf:
 }
 
 int	alx_sscan_uint	(unsigned *restrict dest,
-			unsigned m, unsigned def, unsigned M,
-			const char *restrict str)
+			 unsigned m, unsigned def, unsigned M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %u", dest) != 1)
@@ -115,8 +115,8 @@ err_sscanf:
 }
 
 int	alx_sscan_int	(int *restrict dest,
-			int m, int def, int M,
-			const char *restrict str)
+			 int m, int def, int M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %i", dest) != 1)
@@ -134,8 +134,8 @@ err_sscanf:
 }
 
 int	alx_sscan_u8	(uint8_t *restrict dest,
-			uint8_t m, uint8_t def, uint8_t M,
-			const char *restrict str)
+			 uint8_t m, uint8_t def, uint8_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNu8"", dest) != 1)
@@ -153,8 +153,8 @@ err_sscanf:
 }
 
 int	alx_sscan_s8	(int8_t *restrict dest,
-			int8_t m, int8_t def, int8_t M,
-			const char *restrict str)
+			 int8_t m, int8_t def, int8_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNi8"", dest) != 1)
@@ -172,8 +172,8 @@ err_sscanf:
 }
 
 int	alx_sscan_u16	(uint16_t *restrict dest,
-			uint16_t m, uint16_t def, uint16_t M,
-			const char *restrict str)
+			 uint16_t m, uint16_t def, uint16_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNu16"", dest) != 1)
@@ -191,8 +191,8 @@ err_sscanf:
 }
 
 int	alx_sscan_s16	(int16_t *restrict dest,
-			int16_t m, int16_t def, int16_t M,
-			const char *restrict str)
+			 int16_t m, int16_t def, int16_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNi16"", dest) != 1)
@@ -210,8 +210,8 @@ err_sscanf:
 }
 
 int	alx_sscan_u32	(uint32_t *restrict dest,
-			uint32_t m, uint32_t def, uint32_t M,
-			const char *restrict str)
+			 uint32_t m, uint32_t def, uint32_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNu32"", dest) != 1)
@@ -229,8 +229,8 @@ err_sscanf:
 }
 
 int	alx_sscan_s32	(int32_t *restrict dest,
-			int32_t m, int32_t def, int32_t M,
-			const char *restrict str)
+			 int32_t m, int32_t def, int32_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNi32"", dest) != 1)
@@ -248,8 +248,8 @@ err_sscanf:
 }
 
 int	alx_sscan_u64	(uint64_t *restrict dest,
-			uint64_t m, uint64_t def, uint64_t M,
-			const char *restrict str)
+			 uint64_t m, uint64_t def, uint64_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNu64"", dest) != 1)
@@ -267,8 +267,8 @@ err_sscanf:
 }
 
 int	alx_sscan_s64	(int64_t *restrict dest,
-			int64_t m, int64_t def, int64_t M,
-			const char *restrict str)
+			 int64_t m, int64_t def, int64_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %"SCNi64"", dest) != 1)
@@ -286,8 +286,8 @@ err_sscanf:
 }
 
 int	alx_sscan_pdif	(ptrdiff_t *restrict dest,
-			ptrdiff_t m, ptrdiff_t def, ptrdiff_t M,
-			const char *restrict str)
+			 ptrdiff_t m, ptrdiff_t def, ptrdiff_t M,
+			 const char *restrict str)
 {
 
 	if (sscanf(str, " %ti", dest) != 1)
@@ -305,9 +305,9 @@ err_sscanf:
 }
 
 int	alx_sscan_ch	(char *restrict dest,
-			const char *restrict valid,
-			bool skip_space, bool ignore_case,
-			const char *restrict str)
+			 const char *restrict valid,
+			 bool skip_space, bool ignore_case,
+			 const char *restrict str)
 {
 	char	*format;
 
@@ -337,9 +337,9 @@ err_sscanf:
 }
 
 int	alx_sscan_fname	(const char *restrict path,
-			char fname[restrict FILENAME_MAX],
-			bool exist,
-			const char *restrict str)
+			 char fname[restrict FILENAME_MAX],
+			 bool exist,
+			 const char *restrict str)
 {
 	char	buff [FILENAME_MAX];
 	char	fpath [FILENAME_MAX];
